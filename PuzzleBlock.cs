02b469a66f9476167d16e20861f4bbb973f31863
Юрьев Александр <yuriev200@mail.ru>
2020-06-30 20:52:21 +0300

    v. 20:52

---
+++ PuzzleBlock.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PuzzleFifteen
+{
+    class PuzzleBlock : Button //Inheritance
+
+    {
+        public PuzzleBlock()
+        {
+            InitializePuzzleBlock();
+        }
+
+        private void InitializePuzzleBlock()
+        {
+            this.BackColor = Color.Orange;
+            this.Height = 80;
+            this.Width = 80;
+            this.Text = "0";
+            this.Font = new Font("Tahoma", 17);
+        }
+    }
+}
